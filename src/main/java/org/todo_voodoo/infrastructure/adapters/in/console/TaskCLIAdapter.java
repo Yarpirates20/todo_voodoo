c7606ba1754fede4b9c54fded081817d94861b70
Robert Samoraj <rsamoraj11@gmail.com>
2026-03-09 20:26:23 -0500

    Add infrastructure directories

---
+++ src/main/java/org/todo_voodoo/infrastructure/adapters/in/console/TaskCLIAdapter.java
@@ -0,0 +1,5 @@
+package org.todo_voodoo.infrastructure.adapters.in.console;
+
+public class TaskCLIAdapter
+{
+}
---
+++ src/main/java/org/todo_voodoo/infrastructure/adapters/out/persistance/PostgresTaskRepository.java
@@ -0,0 +1,5 @@
+package org.todo_voodoo.infrastructure.adapters.out.persistance;
+
+public class PostgresTaskRepository
+{
+}
