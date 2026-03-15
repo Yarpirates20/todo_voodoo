8e9bf4ffc59c07ec4b7267379e95f465cd707d11
Robert Samoraj <rsamoraj11@gmail.com>
2026-03-14 20:31:24 -0500

    Add minimal run method for CLIAdapter

--- src/main/java/org/todo_voodoo/infrastructure/adapters/in/console/TaskCLIAdapter.java
+++ src/main/java/org/todo_voodoo/infrastructure/adapters/in/console/TaskCLIAdapter.java
@@ -1,5 +1,61 @@
 package org.todo_voodoo.infrastructure.adapters.in.console;
 
+import org.todo_voodoo.domain.ports.in.TaskUseCase;
+
+import java.util.Scanner;
+
+/**
+ * A basic command-line interface which is an incoming driver of the domain and
+ * handles I/O.
+ */
 public class TaskCLIAdapter
 {
+    private final TaskUseCase taskService;
+    private final Scanner scanner;
+
+
+
+    /**
+     * Constructor.
+     *
+     * @param taskService
+     */
+    public TaskCLIAdapter(TaskUseCase taskService)
+    {
+        this.taskService = taskService;
+        this.scanner = new Scanner(System.in);
+    }
+
+    /**
+     * This method starts the application's interactive CLI loop
+     * and keeps it alive until the user exists.
+     *
+     * The run() method prints the menu, reads the user's choice, and calls
+     * appropriate helper functions.
+     */
+    public void run()
+    {
+        boolean running = true;
+        while (running)
+        {
+            printMenu();
+            String command = scanner.nextLine();
+
+            if (command.equals("exit"))
+                running = false;
+            else
+            {
+                handleCommand(command);
+            }
+        }
+
+    }
+
+    /**
+     *
+     */
+    public void printMenu()
+    {
+    }
+
 }
